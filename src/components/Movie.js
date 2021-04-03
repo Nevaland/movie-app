c7fa2306a3e5bd268a9cec85506962ee0bd53a12
Nevaland <deanh0716@gmail.com>
2021-04-03 13:43:11 +0900

    Fix css and style

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -4,6 +4,12 @@ import PropTypes from "prop-types";
 import "./Movie.css";
 
 function Movie({ id, year, title, summary, poster, genres }) {
+  if (summary.length == 0) {
+    summary = "No Description.";
+  } else if (summary.length > 100) {
+    summary = summary.slice(0, 100) + "...";
+  }
+
   return (
     <div className="movie">
       <Link
@@ -22,14 +28,14 @@ function Movie({ id, year, title, summary, poster, genres }) {
         <div className="movie__data">
           <h3 className="movie__title">{title}</h3>
           <h5 className="movie__year">{year}</h5>
-          <ul className="genres">
+          <ul className="movie__genres">
             {genres.map((genres, index) => (
-              <li key={index} className="genres__genres">
+              <li key={index} className="genres__genre">
                 {genres}
               </li>
             ))}
           </ul>
-          <p className="movie__summary">{summary.slice(0, 180)}...</p>
+          <p className="movie__summary">{summary}</p>
         </div>
       </Link>
     </div>
